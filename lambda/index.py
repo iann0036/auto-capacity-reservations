62dcadde46f18799a5ef4fad7fd8f757f07d81f6
Ian Mckay <contact@ianmckay.com.au>
2020-02-03 00:11:57 +1100

    Finished Lambda logic and deployment template

--- lambda/index.py
+++ lambda/index.py
@@ -1,6 +1,7 @@
 import boto3
 import pprint
 import random
+from datetime import datetime, timedelta, timezone
 
 client = boto3.client('ec2')
 
@@ -11,7 +12,143 @@ def handler(event, context):
         InstanceIds=[instanceid]
     )['Reservations'][0]['Instances'][0]
 
+    platform = "Linux/UNIX"
+    if "Platform" in instance and instance['Platform'].lower() == "windows":
+        platform = "Windows"
+    
+    if 'Tags' in instance:
+        for tag in instance['Tags']:
+            if tag['Key'] == "AutoCapacityReservationInstanceType":
+                platform = tag['Value']
+
     if event['detail']['state'] == "pending":
+        if 'CapacityReservationId' not in instance:
+            capacity_reservations = client.describe_capacity_reservations(
+                Filters=[
+                    {
+                        'Name': 'state',
+                        'Values': [
+                            'active',
+                        ]
+                    },
+                    {
+                        'Name': 'availability-zone',
+                        'Values': [
+                            instance['Placement']['AvailabilityZone'],
+                        ]
+                    },
+                    {
+                        'Name': 'tag:Platform',
+                        'Values': [
+                            platform,
+                        ]
+                    },
+                    {
+                        'Name': 'instance-type',
+                        'Values': [
+                            instance['InstanceType'],
+                        ]
+                    },
+                    {
+                        'Name': 'tenancy',
+                        'Values': [
+                            instance['Placement']['Tenancy'],
+                        ]
+                    },
+                    {
+                        'Name': 'tag-key',
+                        'Values': [
+                            'AutoCapacityReservation',
+                        ]
+                    },
+                ]
+            )['CapacityReservations']
+
+            if len(capacity_reservations) > 0:
+                print("Incrementing capacity reservation: {}".format(capacity_reservations[0]['CapacityReservationId']))
+                client.modify_capacity_reservation(
+                    CapacityReservationId=capacity_reservations[0]['CapacityReservationId'],
+                    InstanceCount=capacity_reservations[0]['TotalInstanceCount'] + 1,
+                )
+            else:
+                print("Creating capacity reservation")
+                client.create_capacity_reservation(
+                    ClientToken=str(random.random())[2:],
+                    InstanceType=instance['InstanceType'],
+                    InstancePlatform=platform,
+                    AvailabilityZone=instance['Placement']['AvailabilityZone'],
+                    Tenancy=instance['Placement']['Tenancy'],
+                    InstanceCount=1,
+                    EndDateType='unlimited',
+                    InstanceMatchCriteria='open',
+                    TagSpecifications=[
+                        {
+                            'ResourceType': 'capacity-reservation',
+                            'Tags': [
+                                {
+                                    'Key': 'AutoCapacityReservation',
+                                    'Value': 'true'
+                                },
+                                {
+                                    'Key': 'Platform',
+                                    'Value': platform
+                                },
+                            ]
+                        },
+                    ]
+                )
+        elif instance['LaunchTime'] > datetime.now(timezone.utc) - timedelta(minutes=1):
+            # if the instance just launched and has claimed a capacity reservation, it's
+            # either claimed a stopped instances spot or has a non-managed reservation
+
+            capacity_reservations = client.describe_capacity_reservations(
+                Filters=[
+                    {
+                        'Name': 'state',
+                        'Values': [
+                            'active',
+                        ]
+                    },
+                    {
+                        'Name': 'availability-zone',
+                        'Values': [
+                            instance['Placement']['AvailabilityZone'],
+                        ]
+                    },
+                    {
+                        'Name': 'tag:Platform',
+                        'Values': [
+                            platform,
+                        ]
+                    },
+                    {
+                        'Name': 'instance-type',
+                        'Values': [
+                            instance['InstanceType'],
+                        ]
+                    },
+                    {
+                        'Name': 'tenancy',
+                        'Values': [
+                            instance['Placement']['Tenancy'],
+                        ]
+                    },
+                    {
+                        'Name': 'tag-key',
+                        'Values': [
+                            'AutoCapacityReservation',
+                        ]
+                    },
+                ]
+            )['CapacityReservations']
+
+            if len(capacity_reservations) > 0 and capacity_reservations[0]['CapacityReservationId'] == instance['CapacityReservationId']:
+                print("Incrementing capacity reservation: {}".format(capacity_reservations[0]['CapacityReservationId']))
+                client.modify_capacity_reservation(
+                    CapacityReservationId=capacity_reservations[0]['CapacityReservationId'],
+                    InstanceCount=capacity_reservations[0]['TotalInstanceCount'] + 1,
+                )
+    elif event['detail']['state'] == "terminated":
         capacity_reservations = client.describe_capacity_reservations(
             Filters=[
                 {
@@ -24,6 +161,12 @@ def handler(event, context):
                     'Name': 'availability-zone',
                     'Values': [
                         instance['Placement']['AvailabilityZone'],
+                    ]
+                },
+                {
+                    'Name': 'tag:Platform',
+                    'Values': [
+                        platform,
                     ]
                 },
                 {
@@ -44,46 +187,20 @@ def handler(event, context):
                         'AutoCapacityReservation',
                     ]
                 },
-                {
-                    'Name': 'tag:ImageId',
-                    'Values': [
-                        instance['ImageId'],
-                    ]
-                },
             ]
         )['CapacityReservations']
 
         if len(capacity_reservations) > 0:
-            pass # TODO
-        else:
-            client.create_capacity_reservation(
-                ClientToken=str(random.random())[2:],
-                InstanceType=instance['InstanceType'],
-                InstancePlatform='Linux/UNIX',
-                AvailabilityZone=instance['Placement']['AvailabilityZone'],
-                Tenancy=instance['Placement']['Tenancy'],
-                InstanceCount=1,
-                #EbsOptimized=True|False,
-                #EphemeralStorage=True|False,
-                EndDateType='unlimited',
-                InstanceMatchCriteria='open',
-                TagSpecifications=[
-                    {
-                        'ResourceType': 'capacity-reservation',
-                        'Tags': [
-                            {
-                                'Key': 'AutoCapacityReservation',
-                                'Value': 'true'
-                            },
-                            {
-                                'Key': 'ImageId',
-                                'Value': instance['ImageId']
-                            },
-                        ]
-                    },
-                ]
-            )
-    elif event['detail']['state'] == "terminated":
-        pass # TODO
+            if capacity_reservations[0]['TotalInstanceCount'] <= 1:
+                print("Cancelling capacity reservation: {}".format(capacity_reservations[0]['CapacityReservationId']))
+                client.cancel_capacity_reservation(
+                    CapacityReservationId=capacity_reservations[0]['CapacityReservationId']
+                )
+            else:
+                print("Decrementing capacity reservation: {}".format(capacity_reservations[0]['CapacityReservationId']))
+                client.modify_capacity_reservation(
+                    CapacityReservationId=capacity_reservations[0]['CapacityReservationId'],
+                    InstanceCount=capacity_reservations[0]['TotalInstanceCount'] - 1,
+                )
     else:
         print("Unhandled state: {}".format(event['detail']['state']))
